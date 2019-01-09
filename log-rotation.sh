8348a195df90ae83a9cf782eae4575f758eee4fd
zaxxas <46424000+zaxxas@users.noreply.github.com>
2019-01-09 10:32:29 +0900

    Change log output.
    If the size of log file is less than the threashold  and the process of compression has not done , log output will not be executed.

--- log-rotation.sh
+++ log-rotation.sh
@@ -3,6 +3,7 @@
 
 ### Constant list ###
 FILE="debug.log"
+#LIMIT_SIZE=`expr 1024  \* 3` #3MB
 LIMIT_SIZE=`expr 1024 \* 1024 \* 3` #3MB
 GZIP_HOME=`which gzip`
 WP_CONTENT_HOME="."
@@ -12,73 +13,87 @@ CF="[CONFIG]"
 
 
 
+### LOG OUTPUT SETTING###
+LOG_BUFFER=""
+function append_log(){
+	LOG_BUFFER="$LOG_BUFFER$1\r\n"
+}
+function print_log(){
+	echo $LOG_BUFFER
+}
+
+
+
 cd ${WP_CONTENT_HOME}
 
 
 
-echo "###### LOG GZIP PROCESS START ###### "
-echo "[DATE]`date`"
+### LOG ###
+append_log "###### LOG GZIP PROCESS START ###### "
+append_log "[DATE]`date`"
 
 
 
-### File exits ###
+### Find File ###
 if [ -e ${FILE} ] ; then
-        echo "${DM}${FILE} Found."
+        append_log "${DM}${FILE} Found."
 else
-        echo "${DM}ERROR:${FILE} Not Found."
-        echo "###### LOG GZIP PROCESS END ###### "
+        append_log "${DM}ERROR:${FILE} Not Found."
+        append_log "###### LOG GZIP PROCESS END ###### "
+	#print_log
         exit 0
 fi
 
 
 
-### decision  ###
+### Decide whether do compression ###
 file_size=`wc -c ${FILE} | awk '{printf $1}'`
-echo "${DM}CURRENT FILE SIZE:${file_size} byte"
+append_log "${DM}CURRENT FILE SIZE:${file_size} byte"
 
 if [ ${file_size} -lt ${LIMIT_SIZE} ] ; then
-        echo "${DM}No Need to gzip now.End."
-        echo "###### LOG GZIP PROCESS END ###### "
+        append_log "${DM}No Need to gzip now.End."
+        append_log "###### LOG GZIP PROCESS END ###### "
+	#print_log
         exit 0
 fi
 
 
 
-### create directory if not exists ###
+### Create directory if not exists ###
 if [ ! -e ${LOG_DIR} ] ; then
         mkdir ${LOG_DIR}
-        echo "${DM}Create \"${LOG_DIR}\" Directory."
+        append_log "${DM}Create \"${LOG_DIR}\" Directory."
 fi
 
 
 
-### decision of number ###
+### Deciside version number ###
 current_file=`ls -r ${LOG_DIR}/${FILE}* | head -1`
-echo "${DM}current_file=$current_file"
-#if [ ${current_file}
+append_log "${DM}current_file=$current_file"
 current_num=`printf %3g ${current_file##${LOG_DIR}/${FILE}.}`
 
 next_num=`expr $current_num + 1`
 next_num=`printf %03g ${next_num}`
-echo "${DM}CUR-NUM=$current_num,NEXT-NUM=$next_num"
+append_log "${DM}CUR-NUM=$current_num,NEXT-NUM=$next_num"
 
 
 
-### do gzip compression ###
+### Do gzip compression ###
 ${GZIP_HOME} ${FILE} -c > ${LOG_DIR}/${FILE}.${next_num}
 cp /dev/null ${FILE}
 
 
 
-### output ###
+### Check if compressed properly ###
 if [ -e ${LOG_DIR}/$FILE.$next_num ] ; then
-        echo "${DM}Done gzip compression. New Create ${FILE}.${next_num}"
+        append_log "${DM}Done gzip compression. New Create ${FILE}.${next_num}"
 else
-        echo "${DM}Fail gzip compression. Cannot create ${FILE}.${next_num}"
+        append_log "${DM}Fail gzip compression. Cannot create ${FILE}.${next_num}"
 fi
 
 
-
-echo "###### LOG GZIP PROCESS END ###### "
+### LOG ###
+append_log "###### LOG GZIP PROCESS END ###### "
+print_log
 exit 0
 
